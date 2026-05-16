a231bbdca0a3130722c459a80df91bb956a9debf
Kundan Gosavi <gosavikundan4@gmail.com>
2026-05-16 18:02:28 +0530

    Add imagekit_services module for file upload and image variant generation

---
+++ Backend/services/imagekit_services.py
@@ -0,0 +1,29 @@
+from imagekitio import ImageKit
+from config import IMAGEKIT_PRIVATE_KEY, IMAGEKIT_PUBLIC_KEY, IMAGEKIT_URL_ENDPOINT
+
+imageKit = ImageKit(private_key=IMAGEKIT_PRIVATE_KEY,)
+
+
+def upload_file(file_bytes: bytes, file_name: str, folder: str, content_type: str = 'jpeg/png'):
+
+    """Uploads a file to ImageKit and returns the URL of the uploaded file."""
+
+    result = imageKit.files.upload(
+        file=(file_bytes, file_name, content_type),
+        file_name=file_name,
+        folder=folder,
+        is_private_file=False,
+        use_unique_file_name=True
+    )
+
+    return result.url
+
+def get_variant(base_url: str):
+    """Return 3 size of image: small, medium, large"""
+
+    return {
+        "youtube" : f"{base_url}?tr=w-1280,h-720,fo-auto",
+        "short" : f"{base_url}?tr=w-400,h-400,fo-auto", 
+        "square" : f"{base_url}?tr=w-400,h-400,fo-auto"
+    }
+
